7e29008f434aa0704809aeefc34117c1cdf72858
VSinche <115099082+Vitocho22@users.noreply.github.com>
2023-11-04 21:03:42 -0500

    Agregar archivos de proyecto.

---
+++ lectorQR/lectorQR/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace lectorQR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushModalAsync(scan);
+
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopModalAsync();
+                    await DisplayAlert("Valor QRCODE", "" + result.Text, "OK");
+                });
+            };
+
+        }
+    }
+}
